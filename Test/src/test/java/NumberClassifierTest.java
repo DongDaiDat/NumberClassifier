068b352ff9395a549af218cbe58e24665a50d5b2
Đồng Đại Đạt <dongdat2208@gmail.com>
2026-04-17 07:46:17 +0700

    Add JUnit tests for statement coverage (fix #1)

--- Test/src/test/java/NumberClassifierTest.java
+++ Test/src/test/java/NumberClassifierTest.java
@@ -4,20 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NumberClassifierTest {
 
     @Test
-    void testMorePositives() {
-        int[] input = {1, 2, -1};
-        assertEquals("More positives", NumberClassifier.classify(input));
-    }
+void testMorePositives() {
+    int[] input = {1, 2, -1};
+    assertEquals("More positives", NumberClassifier.classify(input));
+}
 
-    @Test
-    void testMoreNegatives() {
-        int[] input = {-1, -2, 1};
-        assertEquals("More negatives", NumberClassifier.classify(input));
-    }
+@Test
+void testMoreNegatives() {
+    int[] input = {-1, -2, 1};
+    assertEquals("More negatives", NumberClassifier.classify(input));
+}
 
-    @Test
-    void testEqual() {
-        int[] input = {1, -1};
-        assertEquals("Equal", NumberClassifier.classify(input));
-    }
+@Test
+void testEqual() {
+    int[] input = {1, -1};
+    assertEquals("Equal", NumberClassifier.classify(input));
+}
 }
